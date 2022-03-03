78eae15cde3b78191cd7cd15acf911878741744a
Elijah Johnson <elijohnson565@gmail.com>
2022-03-03 14:03:56 -0800

    add heroku

--- server.js
+++ server.js
@@ -6,20 +6,34 @@ const app = express();
 
 function filterByQuery(query, animalsArray){
 
+    let personalityTraitsArray = [];
+    
     let filteredResults = animalsArray;
+    if (query.personalityTraits) {
 
-    if(query.diet){
-        filteredResults = filteredResults.filter(animal => animal.diet == query.diet)
+      if (typeof query.personalityTraits === 'string') {
+        personalityTraitsArray = [query.personalityTraits];
+      } else {
+        personalityTraitsArray = query.personalityTraits;
+      }
+
+      personalityTraitsArray.forEach(trait => {
+  
+        filteredResults = filteredResults.filter(
+          animal => animal.personalityTraits.indexOf(trait) !== -1
+        );
+      });
     }
-
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
     if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
     }
-
     if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
-
+    // return the filtered results:
     return filteredResults;
 
 }
